6c2946e43d6bb389078354b1b5624a93ff1e7cf6
Sai Krishnanand Nagavelli <nagavellisai@gmail.com>
2017-04-29 22:51:09 -0400

    Cpp file where the function definitions are made

---
+++ Arrow/Arrow.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"Arrow.h"
+using namespace std;
+
+Arrow::Arrow()
+{
+ }
+void Arrow::Print()
+{
+  cout<<"Idi naa maata..!!Naa maate Shaasanam..!!Jai Maahishmathi...!!"<<endl;
+ }
